ec2b7292750dafedf53da051d99c6ffbe2c8ca89
zima563 <mohamedabdelazim654@gmail.com>
2025-07-06 11:28:25 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as dns from 'dns';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
