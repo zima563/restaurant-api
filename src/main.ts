92bbf72b32de51abce7ca5ff3cda371b4f27edea
zima563 <mohamedabdelazim654@gmail.com>
2025-07-06 11:38:25 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ import * as dns from 'dns';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  origin: ["http://localhost:5174", "http://localhost:3000", ...], // ضيف كل origins اللي عندك
-  credentials: true,
-});
+    origin: ['http://localhost:5174', 'http://localhost:3000'], // ضيف كل origins اللي عندك
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
