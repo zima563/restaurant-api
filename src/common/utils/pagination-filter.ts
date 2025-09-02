2bdeb5fafe113b82a5f8d6ef06a1a15cc8255eac
zima563 <mohamedabdelazim654@gmail.com>
2025-09-02 11:33:35 +0300

    remove pagination from products

--- src/common/utils/pagination-filter.ts
+++ src/common/utils/pagination-filter.ts
@@ -1,13 +1,13 @@
 export function buildProductQueryFilters(query: any) {
-  const { categoryId, foodType, page = 1, limit = 10, ...other } = query;
+  const { categoryId, foodType, ...other } = query;
   const where: any = {};
 
   if (categoryId) where.categoryId = +categoryId;
   if (foodType) where.foodType = foodType;
 
-  const take = +limit;
-  const skip = (+page - 1) * take;
+  // const take = +limit;
+  // const skip = (+page - 1) * take;
   const orderBy = { createdAt: 'desc' as const };
 
-  return { where, take, skip, orderBy, page: +page, limit: +limit };
+  return { where, orderBy };
 }
